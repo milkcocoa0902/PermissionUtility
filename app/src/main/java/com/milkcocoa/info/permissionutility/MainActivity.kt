a3bad8e26c2d1a6fceccd02c913d2493cec2ce6f
keita <developer@milkcocoa.info>
2023-12-10 21:52:21 +0900

    add sample

--- app/src/main/java/com/milkcocoa/info/permissionutility/MainActivity.kt
+++ app/src/main/java/com/milkcocoa/info/permissionutility/MainActivity.kt
@@ -1,28 +1,47 @@
 package com.milkcocoa.info.permissionutility
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.milkcocoa.info.permission_util.PermissionResult
 import com.milkcocoa.info.permission_util.RequestPermissionLauncher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val requestCameraPermission: RequestPermissionLauncher =
+            RequestPermissionLauncher(
+                activity = this,
+                permission = Manifest.permission.CAMERA
+            )
 
-        RequestPermissionLauncher(
-            this,
-            permission = android.Manifest.permission.CAMERA
-        ).let {
-            CoroutineScope(Dispatchers.Main).launch {
-                it.launch().let {
-                    Log.i("MainActivity", it.toString())
+        Log.i("MainActivity", requestCameraPermission.checkSelfPermission(this).toString())
+
+        CoroutineScope(Dispatchers.Main).launch {
+            runCatching {
+                requestCameraPermission.launch().let { result ->
+                    Log.i("MainActivity", result.toString())
+                    when(result){
+                        PermissionResult.PERMISSION_GRANTED ->{
+
+                        }
+                        PermissionResult.PERMISSION_DENIED ->{
+
+                        }
+                        PermissionResult.PERMISSION_ABSOLUTELY_DENIED ->{
+
+                        }
+                    }
                 }
-            }
+            }.getOrNull()
         }
 
     }
